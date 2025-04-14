a8164e06934c53643877151e324bffe4d331170f
MKlip <mklipfel@gmx.de>
2025-04-14 21:49:41 +0200

    Add HTML Report to MainRunner

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -4,6 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
-        tags = "@main", monochrome = false,dryRun = false)
+        tags = "@main", monochrome = false,dryRun = false,
+        plugin = {"pretty", "html:target/cucumber"})
 public class MainRunner extends AbstractTestNGCucumberTests {
 }
