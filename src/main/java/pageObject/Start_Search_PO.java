9023bb3d3ddce2fd8129138b91211031e99d47b0
MKlip <mklipfel@gmx.de>
2025-04-14 21:22:11 +0200

    Fix Selector for search bar category buttons

--- src/main/java/pageObject/Start_Search_PO.java
+++ src/main/java/pageObject/Start_Search_PO.java
@@ -14,14 +14,17 @@ public class Start_Search_PO extends Base_PO{
     private @FindBy(id = "search17")
     WebElement search_Bar_TextField;
 
-    private @FindBy(id = "deichmann-de-women_19")
+    private @FindBy(id = "label[for='deichmann-de-women_19']")
     WebElement search_Bar_Women_Button;
 
-    private @FindBy(id = "deichmann-de-men_19")
+    private @FindBy(css = "label[for='deichmann-de-men_19']")
     WebElement search_Bar_Men_Button;
 
-    private @FindBy(id = "deichmann-de-children_19")
+    private @FindBy(css = "label[for='deichmann-de-children_19']")
     WebElement search_Bar_Children_Button;
+
+    private @FindBy(className = "suggestion-link")
+    WebElement search_Bar_suggestion;
 
     public Start_Search_PO() { super();}
 
@@ -47,6 +50,10 @@ public class Start_Search_PO extends Base_PO{
 
     public void clickUnique_Category() {
         waitForWebElementAndClick(search_Bar_Men_Button);
+    }
+
+    public void clickFirst_Suggestion() {
+        waitForWebElementAndClick(search_Bar_suggestion);
     }
 
 }
